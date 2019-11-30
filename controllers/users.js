9dabd6e9471fa2a5d8c65c378c35bed9a38c8118
utkarsh-rai <utkarshr2811@gmail.com>
2019-11-30 17:40:01 +0530

    Added server.js and supportive frameworks

---
+++ controllers/users.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function(_){
+
+    return {
+        SetRouting: function(router){
+            router.get('/', this.indexPage);
+            router.get('/signup', this.getSignUp);
+        },
+
+        indexPage: function(req, res){
+            return res.render('index');
+        },
+
+        getSignUp: function(req, res){
+            return res.render('signup');
+        }
+    }
+    
+}
